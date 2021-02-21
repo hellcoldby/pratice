ab028f547f42246e5ea158a13ff048c59842ec95
hellcoldby <coldbao@gmail.com>
2021-02-22 00:09:12 +0800

    add call

--- write_apply.js
+++ write_apply.js
@@ -3,6 +3,22 @@
  * @Author: ygp
  * @Date: 2021-01-16 19:06:06
  * @LastEditors: ygp
- * @LastEditTime: 2021-01-16 20:20:52
+ * @LastEditTime: 2021-02-21 23:21:04
  */
-function toApply(){}
+function toApply(fn, arr){
+    fn = fn? Object(fn) : window;
+    fn.con = this;
+    let args = [];
+   
+    let res = null;
+    if(!arr){
+        res = fn.con();
+    }else{
+        for(let i=1; i<arr.length; i++){
+            args.push(arr[i]+'');
+        }
+        res =  eval('fn.con('+ args +')');
+    }
+    delete fn.con;
+    return res;
+}
--- write_call.js
+++ write_call.js
@@ -3,5 +3,17 @@
  * @Author: ygp
  * @Date: 2021-02-21 19:06:19
  * @LastEditors: ygp
- * @LastEditTime: 2021-02-21 19:41:00
+ * @LastEditTime: 2021-02-21 23:18:19
  */
+
+ Function.prototype.toCall = function(fn){
+     fn = fn? Object(fn): window;
+     fn.con = this;
+     let args = [];
+     for(let i=1; i<arguments.length; i++){
+        args.push(arguments[i] + '');
+     }
+     const res =  eval('fn.con('+args+')');
+     delete fn.con;
+     return res;
+ }
