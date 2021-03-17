c24c05b99be5a058c7a13977038915aac2cfc812
hellcoldby <coldbao@gmail.com>
2021-03-17 16:16:51 +0800

    fix new error

--- write_new.js
+++ write_new.js
@@ -3,16 +3,15 @@
  * @Author: ygp
  * @Date: 2021-02-21 19:06:19
  * @LastEditors: ygp
- * @LastEditTime: 2021-02-21 19:39:07
+ * @LastEditTime: 2021-03-17 15:41:16
  */
 
 // 模拟实现一个 new 函数  new fn() --> toNew(fn)
  function toNew(){
-    const fn = [].shift.call(arguments);
-
     let obj = Object.create(null);
-    const res = obj.apply(fn, arguments);
-
+    const fn = [].shift.call(arguments);
     obj.prototype = fn.prototype;
+    const res = fn.apply(obj, arguments);
+
     return res instanceof Object? res: obj;
  }
