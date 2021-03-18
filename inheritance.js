fc302f64e2f8ef005aad168d99a69be2ae8e4cfe
hellcoldby <coldbao@gmail.com>
2021-03-18 16:53:16 +0800

    继承和new

--- inheritance.js
+++ inheritance.js
@@ -3,13 +3,33 @@
  * @Author: ygp
  * @Date: 2021-03-18 12:19:12
  * @LastEditors: ygp
- * @LastEditTime: 2021-03-18 14:40:08
+ * @LastEditTime: 2021-03-18 16:46:33
  */
+
+// 先对比一下 new  和 继承的 核心思路
+
+/**
+ *  1. new --- 当前函数
+ *  - 1.1 创建空对象
+ *  - 1.2 当前函数this 指向空对象
+ *  - 1.3 空对象挂载当前函数的所有扩展方法
+ * 
+ * 
+ *  2. 继承 --- 当前函数
+ *  - 2.1 创建子函数
+ *  - 2.2 当前函数的this 指向子函数
+ *  - 2.3 子函数挂载当前函数所有的扩展方法
+ */
+
+// 对比之后你会发现：new 和 继承 的思路是一样的
+// 不同之处在于，
+// new 是内部函数内部创建一个空对象返回一个新对象，  
+// 继承是你自己创建的函数，复用父级函数的属性方法
+
 
 /** 
  * 继承有两类： 对象继承 类型继承
  */
-
 //1. 对象继承 --- 用Object.create();
 let person = {name: 'jack'}
 // 相当于 p1.prototype = person.prototype
@@ -45,3 +65,17 @@ function extend(parent, child) {
     }
     return child;
 }
+
+//5. 混合继承
+function MyClass() {
+    SuperClass.call(this);
+    OtherSuperClass.call(this);
+}
+
+// 继承一个类
+MyClass.prototype = Object.create(SuperClass.prototype);
+// 混合其它
+// Object.assign 会把  OtherSuperClass原型上的函数拷贝到 MyClass原型上
+Object.assign(MyClass.prototype, OtherSuperClass.prototype);
+// 重新指定constructor
+MyClass.prototype.constructor = MyClass;
