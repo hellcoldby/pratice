275b87c2ef4c92df42034f196c822987733db0d1
hellcoldby <coldbao@gmail.com>
2021-03-18 11:37:35 +0800

    增加bind

--- stroy.js
+++ stroy.js
@@ -3,7 +3,7 @@
  * @Author: ygp
  * @Date: 2021-03-17 16:53:19
  * @LastEditors: ygp
- * @LastEditTime: 2021-03-17 17:31:46
+ * @LastEditTime: 2021-03-18 11:28:39
  */
 
 //你发明了一个暴富提款机
@@ -78,4 +78,68 @@ Function.prototype.toApply = function(obj, arr){
     // delete obj.mac; //3. 删除机器的归属
     // return res;
 
+}
+
+// 还有，call 和 apply 都是立即执行， 能不能先把钱放进去，我说开始生产，然后再吐呢
+magic.bind(obj, 50);
+
+//思路: 返回一个可执行的函数，里边是执行的动作
+Function.prototype.toBind = function(obj){
+    const _this = this;
+    return function(){
+        _this.call(obj)
+    }
+}
+
+//参数处理
+Function.prototype.toBind = function(obj){
+
+    let args = [].slice.call(arguments, 1); //从第二个参数开始获取
+
+    // const _this = this;
+    // return function(){
+            const bindArgs = [].slice.call(arguments);
+            const mergeArgs = args.concat() //合并参数
+             _this.apply(obj, mergeArgs);
+    // }
+}
+
+//之前的机器，还有一些扩展的功能，例如：发现你的钱超过10000会播放歌曲
+magic.prototype.ext = function(){
+    if(this.money>=10000){
+        console.log('lalalalala');
+    }
+}
+
+//toBind 返回的可执行函数 需要继承这些功能才行
+Function.prototype.toBind = function(obj){
+
+    let args = [].slice.call(arguments, 1); //从第二个参数开始获取
+
+    // const _this = this;
+    const bindFn = function(){
+            const bindArgs = [].slice.call(arguments);
+            const mergeArgs = args.concat() //合并参数
+             _this.apply(obj, mergeArgs);
+    }
+
+    bindFn.prototype = this.prototype;   //!!!! 这里的继承有个问题，都指向同一个地址，容易被修改
+    return bindFn;
+}
+
+//注意啊： !!!! 这里的继承有个问题，都指向同一个地址，容易被修改
+Function.prototype.toBind = function(obj){
+
+    let args = [].slice.call(arguments, 1); //从第二个参数开始获取
+
+    // const _this = this;
+    const bindFn = function(){
+            const bindArgs = [].slice.call(arguments);
+            const mergeArgs = args.concat() //合并参数
+             _this.apply(obj, mergeArgs);
+    }
+    const tmpFn = function(){};
+    tmpFn.prototype = this.prototype;
+    bindFn.prototype = new tmpFn();   //!!!! 这里的继承有个问题，都指向同一个地址，容易被修改
+    return bindFn;
 }
--- write_bind.js
+++ write_bind.js
@@ -3,9 +3,42 @@
  * @Author: ygp
  * @Date: 2021-01-16 19:06:14
  * @LastEditors: ygp
- * @LastEditTime: 2021-01-16 20:20:16
+ * @LastEditTime: 2021-03-18 11:37:19
  */
 
-function toBind(){
+//思路： 返回一个等待执行的函数
+function toBind(obj){
+    const _this = this;
+    return function(){
+        obj.call(_this)
+    }
+}
+//参数
+function toBind(obj){
     
+    // const _this = this;
+    const args = [].slice.call(arguments,1);
+
+    // return function(){
+        const bindArgs = [].slice.call(arguments);
+        const concatArgs = args.concat(bindArgs);
+    //     obj.call(_this, concatArgs);
+    // }
+}
+//继承
+function toBind(obj){
+    
+    // const _this = this;
+    const args = [].slice.call(arguments,1);
+
+    const fn = function(){
+        const bindArgs = [].slice.call(arguments);
+        const concatArgs = args.concat(bindArgs);
+    //     obj.call(_this, concatArgs);
+    }
+
+    function tmpFn(){};
+    tmpFn.prototype = this.prototype;
+    fn.prototype = new tmpFn();
+    return fn;
 }
