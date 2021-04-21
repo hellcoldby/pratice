16cf4dc36f5fbd5b6b97aae4d53ed15cd5805b84
hellcoldby <coldbao@gmail.com>
2021-04-21 13:49:56 +0800

    add Array

--- write_ArrayMap.js
+++ write_ArrayMap.js
@@ -3,7 +3,7 @@
  * @Author: ygp
  * @Date: 2021-04-20 17:07:06
  * @LastEditors: ygp
- * @LastEditTime: 2021-04-20 17:44:43
+ * @LastEditTime: 2021-04-21 13:23:55
  */
 
 // 返回新数组
@@ -17,7 +17,7 @@ Array.prototype.MyMap = function(fn, context){
 }
 
 // 返回条件符合的数组
-Array.prototype.Myfilter = function(fn, context){
+Array.prototype.MyFilter = function(fn, context){
     var arr = Array.prototype.slice.call(this);
     var filterArr = [];
     for(var i=0; i<arr.length; i++){
@@ -29,7 +29,7 @@ Array.prototype.Myfilter = function(fn, context){
 }
 
 // 类数组转换为数组
-Array.prototype.from = function(fn, context){
+Array.prototype.myForm = function(fn, context){
     var arr = Array.prototype.slice.call(this);
     if(fn){
         var mappedArr = [];
@@ -40,3 +40,14 @@ Array.prototype.from = function(fn, context){
     }
 }
 
+
+Array.prototype.myReduce = function(fn, initValue){
+    var arr = Array.prototype.slice.call(this);
+    var res, startIndex;
+    res = initValue? initValue: arr[0];
+    startIndex  = initValue? 0 : 1;
+    for(var i = startIndex; i < arr.length; i++){
+        res = fn.call(null, res, arr[i], i, this);
+    }
+    return res;
+}
