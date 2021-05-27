0fdab5f4caeae5a7558ba8d8a993fdade0d8b9b9
hellcoldby <coldbao@gmail.com>
2021-05-28 01:26:49 +0800

    test MyPromise

--- test.js
+++ test.js
@@ -3,29 +3,46 @@
  * @Author: ygp
  * @Date: 2021-05-25 23:35:53
  * @LastEditors: ygp
- * @LastEditTime: 2021-05-26 00:37:35
+ * @LastEditTime: 2021-05-28 01:26:02
  */
 
 
 function MyPromise(fn){
+    this.value = null;
+    this.status = 'pending'; //等待中。。。
     fn(this.resolve.bind(this), this.reject.bind(this));
 }
 
 MyPromise.prototype.resolve = function(val){
-  console.log(val);
+  this.status = 'fulfilled'; //完成
+  this.value = val;
 };
 MyPromise.prototype.reject = function(err){
-  console.log(err);
+  this.status = 'rejected'; //失败
+  this.value = err;
 };
-MyPromise.prototype.then = function(_val){
-  return new Promise((_res, _rej)=>{
+MyPromise.prototype.then = function(onFulfilled, onRejected){
 
-  })
+  return new MyPromise((nextResolve, nextRejected) => {
+    switch(this.status){
+      case 'pending':
+        break;
+      case 'fulfilled':
+        onFulfilled(this.value);
+        break;
+      case 'rejected':
+        onRejected(this.value);
+        break;
+    }
+  });
+  
 }
 
 
 new MyPromise((res, rej) =>{
   res('ok');
+}).then(res => {
+  console.log(res);
 })
 
 
