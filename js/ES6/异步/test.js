fccd71a16269a8bd03aa666fc61878668d74d6eb
hellcoldby <coldbao@gmail.com>
2021-09-09 15:58:36 +0800

    实现promise.all

--- js/ES6/异步/test.js
+++ js/ES6/异步/test.js
@@ -13,33 +13,68 @@ const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
 class Promise{
+
+    static resolve(value){
+        return new Promise((resolve, reject)=>{
+            resolve(value);
+        });
+    }
+
+    static reject(reason){
+        return new Promise ((reason, reject)=>{
+            reject(reason);
+        })
+    }
+
     constructor(fn){
         this.value = null;
         this.error = null;
         this.state = PENDING;
+        this.onFulfilled_ary = [];
+        this.onRejected_ary = [];
 
-        this.resolve = (val)=>{
-        //    console.log('解决',val);
+        let resolve = (val)=>{
+            
+            if(val instanceof Promise){
+                return  val.then(resolve, reject)
+            }
+
            this.value = val;
            this.state = FULFILLED;
+           this.onFulfilled_ary.forEach(fn=>fn());
         }
 
-        this.rejected = (error)=>{
+        let reject = (error)=>{
             // console.log('错误', error);
             this.error = error;
             this.state = REJECTED;
         }
 
-        fn(this.resolve, this.rejected);
+        try{
+            fn(resolve, reject);
+        }catch(err){
+            reject(err);
+        }
     }
 
     then(onFulfilled, onRejected){
-        //需要返回一个新的 promise 才能链式调用
+     
         return new Promise((resolve, reject)=>{
-            //检测履行承诺的结果
+
+            if(this.state === PENDING){
+                this.onFulfilled_ary.push(()=>{
+                    let x = onFulfilled(this.value);
+                    checkPromise(x, resolve, reject);
+                });
+
+                this.onRejected_ary.push(()=>{
+                    let x = onRejected(this.error);
+                    checkPromise(x, resolve, reject);
+                })
+            }
+
             if(this.state === FULFILLED){
                let x = onFulfilled(this.value);
-               //如果 x 是个 promise,等成功执行完成，后在then中履行当前的承诺
                checkPromise(x, resolve, reject);
             }
             if(this.state === REJECTED){
@@ -49,6 +84,48 @@ class Promise{
 
         });
     }
+
+    catch (errCallback) {
+        return this.then(null, errCallback);
+    }
+
+    finally(callback){
+        return this.then(value => {
+            return Promise.resolve(callback()).then(()=>value)
+        }, reason=>{
+            return Promise.resolve(callback()).then(()=>{throw reason})
+        });
+    }
+
+
+    //all() 将多个Promise 实例，包装成一个新的Promise 实例。
+    //接收一个数组 或者 具有iterator 接口作为参数，  而且成员都是promise实例。
+    // 如果成员不是Promise实例，就用promise的resolve方法，将参数转为 Promise 实例。
+    all(ary) {
+
+        if(ary[Symbol.iterator] === undefined){
+            const type = typeof ary;
+            return new TypeError (`TypeError: ${type} ${ary} is not iterator`);
+        }
+
+        let newArray=[];
+        let count = 0;
+        let len = ary.length;
+        return new Promise((resolve, reject)=>{
+            for(let index in  ary){
+                // 转化为promise 对象
+                Promise.resolve(ary[index])
+                .then(res=>{
+                    newArray[index] = res;
+                    if(++count === len){
+                        resolve(newArray);
+                    }
+                }, err=>{
+                    return reject(err);
+                })
+            }   
+        })
+    }
 }
 
 
@@ -56,6 +133,8 @@ function checkPromise(x, resolve, reject){
     if(x instanceof Promise){
         x.then(res=>{
             checkPromise(res, resolve, reject);
+        }, rej=>{
+            reject(rej);
         })
     }else{
         resolve(x)
@@ -63,19 +142,3 @@ function checkPromise(x, resolve, reject){
 }
 
 
-
-
-
-
-const pr = new Promise (function(resolve, rejected){
-    resolve('im----ok');
-});
-pr.then(res=>{
-    console.log('then---',res);
-    return new Promise(resolve=>{
-        resolve('hello');
-    });
-    // return 'hello'
-}).then(res=>{
-    console.log(res);
-})
