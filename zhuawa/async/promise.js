15929e4202dc24d5865c49c47f6f02b096b23a2b
zen <coldbao@gmail.com>
2021-10-03 19:35:57 +0800

    merge dev

--- zhuawa/async/promise.js
+++ zhuawa/async/promise.js
@@ -1,39 +1,75 @@
 class Promise{
     constructor(fn){
-        
+        this.statue = 'pending'
         this.value = null;
         this.reason = null;
-        
-        this.status = 'pending';
-
-        this.resolve = value =>{
-            this.status = 'success';
+        this.resolve = (value)=>{
             this.value = value;
         }
-
-        this.reject = reason =>{
-            this.status = 'failed';
+        this.reject = (reason)=>{
             this.reason = reason;
         }
-
-        fn(this.resolve, this.reject);
+        fn(this.resolve, this.reject)
     }
 
     then(onFulfilled, onRejected){
-        
-        onFulfilled = typeof onFulfilled === 'function'? onFulfilled: v=> v;
+        const onFulfilled = typeof onFulfilled === 'function'? onFulfilled : v => v;
+        const onRejected = typeof onRejected === 'function'? onRejected : v=> v;
 
-        return new Promise((resolve, reject)=>{
-            if(this.status === 'success'){
-               const res =  onFulfilled(this.value);
-               resolve(res)
+        return new Promise((_resolve, _reject)=>{
+            if(this.statue === 'success'){
+               const res = onFulfilled(this.value);
+               checkRes(res, _resolve, _reject);
+               
             }
-
-            if(this.status === 'failed'){
-                const res = onRejected(this.reason);
-                reject(res);
+    
+            if(this.statue === 'fail'){
+                const res = onRejected(this.reject);
+                checkRes(res, _resolve, _reject);
             }
             
         })
+
     }
+
+    static resolve (value){
+        return new Promise(resolve => {
+            resolve(value);
+        });
+    }
+
+    static reject (reason){
+        return new Promise(reject=>{
+            reject(reason);
+        })
+    }
+}
+
+
+function checkRes(res, _resolve, _reject) {
+    if(res instanceof Promise){
+        res.then(value => {
+            checkRes(value, _resolve, _reject);
+        });
+    }else if(typeof x === 'object'  &&  typeof x !== null || typeof x === 'function'){
+
+        try {
+            let then = x.then;
+            if(typeof then === 'function'){
+                then(y=>{
+                    checkRes(y, _resolve, _reject);
+                }, z=>{
+                    _reject(z);
+                })
+            }
+        }catch(err){
+            _reject(err);
+        }
+
+       
+    }else{
+        _resolve(res);
+    }
+
+
 }
