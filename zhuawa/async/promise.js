60bd896b8c8e0bef04beb2f9e35f11cc99768e35
hellcoldby <coldbao@gmail.com>
2021-10-08 10:50:59 +0800

    test

--- zhuawa/async/promise.js
+++ zhuawa/async/promise.js
@@ -1,36 +1,35 @@
-class Promise{
-    constructor(fn){
-        
+class Promise {
+    constructor(fn) {
         this.value = null;
         this.reason = null;
 
-        this.resolve = (value) =>{
+        this.resolve = (value) => {
             this.value = value;
-        }   
-        this.reject = (reason) =>{
+        };
+        this.reject = (reason) => {
             this.reason = reason;
-        }
+        };
         fn(this.resolve, this.reject);
     }
 
-    then(onFulfilled, onRejected){
-        //需要先判断 onFulfilled 的类型
+    then(onFulfilled, onRejected) {
+        //这里需要先判断 onFulfilled 的类型
 
-        return new Promise((_resolve) =>{
-            const success =  onFulfilled(this.value); // 需要判断 返回值的类型，方便链式调用
+        return new Promise((_resolve) => {
+            // 需要判断 返回值的类型，方便链式调用
+            const success = onFulfilled(this.value);
             _resolve(success);
         });
-
     }
 }
 
-const pr = new Promise(resolve=>{
-    resolve('hello');
+const pr = new Promise((resolve) => {
+    resolve("hello");
 });
 
-pr.then(val => {
+pr.then((val) => {
     console.log(val);
-    return 'world'
-}).then(val => {
+    return "world";
+}).then((val) => {
     console.log(val);
-})
+});
---
+++ 设计模式/装饰器模式.js
@@ -0,0 +1,7 @@
+/**
+ * 装饰器模式
+ *
+ * 常见的HOC 高阶组件就是装饰器模式
+ */
+
+import React from "react";
