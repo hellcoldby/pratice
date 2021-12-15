99ea68fdc3fa37642fb39dff55c1aeca3f562fae
hellcoldby <coldbao@gmail.com>
2021-12-15 13:33:38 +0800

    add typescript

--- js/call/write_copy.js
+++ js/call/write_copy.js
@@ -19,38 +19,7 @@ function extendCopy(parent){
 
 }
 
-//2. 深拷贝
-function deepClone(parent){
-    if(!parent) return parent;
-    // 如果是数组
-    let child = null;
-    if(Object.prototype.toString.call(parent) === '[object Array]' ){
-        if(parent.length){
-            child = [];
-            for(let i=0; i<parent.length; i++){
-                child[i] = deepClone(parent[i]) ;
-            }
-        }else{
-            child = parent;
-        }
-        return child;
-    }
-    // 如果是对象
-    if(Object.prototype.toString.call(parent) === '[object Object]'){
-        if(JSON.stringify(parent) === '{}'){
-            child = {}
-            for(let key in parent){
-                child[key] = deepClone(parent[key]);
-            }
-        }else{
-            child = parent;
-        }
-        return child;
-    }
-    
-    child = parent;
-    return child;
-}
+//2. 深拷贝 todo
 
 let a = [1,3,5];
 let b = {a: 1, b: 2, c: 3};
