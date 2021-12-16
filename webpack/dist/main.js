3bed93869315d6a2df308d577169da2937573249
zen <coldbao@gmail.com>
2021-12-16 14:22:37 +0800

    add webpack

---
+++ webpack/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./webpack/src/index.js":
+/*!******************************!*\
+  !*** ./webpack/src/index.js ***!
+  \******************************/
+/***/ (() => {
+
+eval("// import _ from 'lodash';\n\nfunction component() {\n    let element = document.createElement('div');\n  \n    // lodash（目前通过一个 script 引入）对于执行这一行是必需的\n    element.innerHTML = 'hello world';\n  \n    return element;\n  }\n  \n  document.body.appendChild(component());\n\n//# sourceURL=webpack://practice/./webpack/src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./webpack/src/index.js"]();
+/******/ 	
+/******/ })()
+;
