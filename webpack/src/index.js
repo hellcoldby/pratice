c2d39092ffd3b9697971805f9eafa9b59b970c44
zen <coldbao@gmail.com>
2022-01-16 19:14:26 +0800

    test webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,11 +1,24 @@
-// import _ from 'lodash';
+// import './style.css';
+// import Icon from './icon.jpg'
+import printMe from './print.js';
+
+
+
+
+// const myIcon = new Image();
+// myIcon.src = Icon;
 
 function component() {
     let element = document.createElement('div');
-  
-   
     element.innerHTML = 'hello world';
-  
+    // element.classList.add('hello');
+    // element.appendChild(myIcon);
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
     return element;
   }
   
