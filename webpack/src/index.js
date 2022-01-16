3a8dfcdffc2b1104ea6e6eb0fa56cb46dad7bbb6
zen <coldbao@gmail.com>
2022-01-16 21:47:33 +0800

    test

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,24 +2,31 @@
 // import Icon from './icon.jpg'
 import printMe from './print.js';
 
+class Book {
+  title = 'a';
 
+  static cover;
+}
+
+const a = new Book();
+a.title = 'The Song of ice and fire';
 
 
 // const myIcon = new Image();
 // myIcon.src = Icon;
 
-function component() {
-    let element = document.createElement('div');
-    element.innerHTML = 'hello world';
+// function component() {
+    // let element = document.createElement('div');
+    // element.innerHTML = 'hello world';
     // element.classList.add('hello');
     // element.appendChild(myIcon);
 
-    const btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
+    // const btn = document.createElement('button');
+    // btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;
+    // element.appendChild(btn);
 
-    return element;
-  }
+  //   return element;
+  // }
   
-  document.body.appendChild(component());
+  // document.body.appendChild(component());
