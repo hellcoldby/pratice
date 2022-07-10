ef79078e7e1937a528787a41ab88530d3a8023d0
zen <coldbao@gmail.com>
2022-07-10 22:40:35 +0800

    test

--- my-react/src/store/redux-saga/runSaga.js
+++
@@ -1,9 +0,0 @@
-export function runSaga({channel, dispatch, getState}, saga){
-    let iterator = saga();
-    const env = {
-        channel,
-        dispatch,
-        getState 
-    }
-    proc (env, iterator)
-}
