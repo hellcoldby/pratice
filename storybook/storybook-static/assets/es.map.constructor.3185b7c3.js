e27c5f613cdf2d9fbbe7b90ab5c47aba3adfb06d
AN <guoping.yang@raykite.com>
2022-08-01 19:45:17 +0800

    test storybook

--- storybook/storybook-static/assets/es.map.constructor.3185b7c3.js
+++
@@ -1,2 +0,0 @@
-import{n as o,o as t}from"./iframe.b0ef78c2.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
-//# sourceMappingURL=es.map.constructor.3185b7c3.js.map
