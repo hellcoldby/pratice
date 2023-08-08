2b358afd7be084b6ef2c5f54b8ac375298f471e8
unknown <guopingyang.tencent.com>
2023-08-08 16:50:01 +0800

    测试小程序条件渲染

--- weChartPro/miniprogram/pages/index/index.ts
+++ weChartPro/miniprogram/pages/index/index.ts
@@ -1,41 +1,38 @@
-// index.ts
-// 获取应用实例
-// const app = getApp<IAppOption>()
+interface TinitToPage{
+  onLoad:(e:any)=>void;
+  switch:(e:any)=>void;
+  addToFront:(e:any)=>void;
+  subtractToFront:(e:any)=>void;
+  addNumberToFront:(e:any)=>void;
+}
+interface TinitToData{
+  [key:string]:any
+}
 
-// Component({
-//   data: {
-//     motto: '1111111111111111111',
-//     time: (new Date()).toString,
-//     hasUserInfo: true,
-//     canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-//     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
-//   },
-//   methods: {
-//     // 事件处理函数
-//     bindViewTap() {
-//       wx.navigateTo({
-//         url: '../logs/logs',
-//       })
-//     },
-//     getUserProfile() {
-//       // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-//       wx.getUserProfile({
-//         desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-//         success: (res) => {
-//           console.log(res)
-//           this.setData({
-//             userInfo: res.userInfo,
-//             hasUserInfo: true
-//           })
-//         }
-//       })
-//     },
-//   },
-// })
-
-Page({
+Page<TinitToData, TinitToPage>({
   data:{
-    time: ''
+    time: '',
+    msg: 'Hello world',
+    a:1,
+    b:2,
+    c:3,
+    len:4,
+    array: [
+      {
+        message:'foo',
+      },{
+        message:'bar'
+      }
+    ],
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4]
   },
   onLoad: function () {
     setInterval(() => {
@@ -52,5 +49,41 @@ Page({
         time: dateTime
       });
     }, 1000);
+  },
+
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  subtractToFront: function(){
+    const length = this.data.objectArray.length
+    if(length>1){
+      this.data.objectArray.shift();
+      this.setData({
+        objectArray:  this.data.objectArray
+      })
+    }
+  },
+  addNumberToFront: function(e){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
   }
 })
